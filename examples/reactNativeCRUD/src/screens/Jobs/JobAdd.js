64c49818a3e5025995569226725f10c53f92cc2d
Dimka Vasilyev <raoffonom@icloud.com>
2019-12-02 14:27:58 +0300

    delete helper.js

--- examples/reactNativeCRUD/src/screens/Jobs/JobAdd.js
+++ examples/reactNativeCRUD/src/screens/Jobs/JobAdd.js
@@ -1,26 +1,32 @@
-import React, { useRef, useReducer, useEffect, useState } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import t from 'tcomb-form-native'
 import { AppContainer, Card, Button, Space, TextLink } from '../../components'
 import { structJob, options } from '../Authenticator/Form'
 import { goBack, PINK } from '../../constants'
 import { createJob, updateJob, deleteJob } from '../../graphql/mutations'
 import { useMutation } from 'aws-amplify-react-hooks'
-import { initialState, reducer } from './helper'
 
 const Form = t.form.Form // eslint-disable-line
 
 const JobAdd = ({ navigation }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const onChange = item => dispatch({ type: 'SET_INPUT', item })
   const [check, setOwner] = useState(false)
+  const [input, setJob] = useState({
+    input: {
+      position: '',
+      rate: '',
+      description: ''
+    }
+  })
+
+  const onChange = item => setJob(item)
+
   useEffect(() => {
     const obj = navigation.state.params
     typeof obj !== 'undefined' && setOwner(true)
-    dispatch({ type: 'CREATE', obj })
+    setJob(obj)
     // FIXME
   }, []) //eslint-disable-line
 
-  const { input } = state
   const [create, update, del, { loading, error }] = useMutation(input)
   const onCreate = () => create(createJob) && goBack(navigation)()
   const onUpdate = () => update(updateJob) && goBack(navigation)()
