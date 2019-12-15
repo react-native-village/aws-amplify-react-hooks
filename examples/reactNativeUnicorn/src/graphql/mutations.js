d3f261b6f296acaec9c040b8ac99a69566afed22
aero <aeroluxx@mail.ru>
2019-12-15 16:28:56 +0300

    amplify api added

---
+++ examples/reactNativeUnicorn/src/graphql/mutations.js
@@ -0,0 +1,42 @@
+// @flow
+// this is an auto generated file. This will be overwritten
+
+export const createJob = `mutation CreateJob(
+  $input: CreateJobInput!
+  $condition: ModelJobConditionInput
+) {
+  createJob(input: $input, condition: $condition) {
+    id
+    position
+    rate
+    description
+    owner
+  }
+}
+`;
+export const updateJob = `mutation UpdateJob(
+  $input: UpdateJobInput!
+  $condition: ModelJobConditionInput
+) {
+  updateJob(input: $input, condition: $condition) {
+    id
+    position
+    rate
+    description
+    owner
+  }
+}
+`;
+export const deleteJob = `mutation DeleteJob(
+  $input: DeleteJobInput!
+  $condition: ModelJobConditionInput
+) {
+  deleteJob(input: $input, condition: $condition) {
+    id
+    position
+    rate
+    description
+    owner
+  }
+}
+`;
