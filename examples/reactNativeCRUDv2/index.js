da2929b6fe6a3b0f13d1d138089a422833baf2b1
Dimka Vasilyev <raoffonom@icloud.com>
2020-03-30 15:56:29 +0300

    auth init

--- examples/reactNativeCRUDv2/index.js
+++ examples/reactNativeCRUDv2/index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
+import { AppRegistry, YellowBox } from 'react-native'
+import App from './src'
+import { name as appName } from './app.json'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+YellowBox.ignoreWarnings([
+  'Warning: componentWillReceiveProps',
+  'RCTRootView cancelTouches',
+  'not authenticated',
+  'Sending `onAnimatedValueUpdate`'
+])
 
-AppRegistry.registerComponent(appName, () => App);
+//window.LOG_LEVEL = 'DEBUG'
+
+AppRegistry.registerComponent(appName, () => App)
---
+++ examples/reactNativeCRUDv2/src/index.js
@@ -0,0 +1,46 @@
+// @flow
+import React from 'react'
+import { StatusBar } from 'react-native'
+import Amplify from '@aws-amplify/core'
+import { Authenticator } from 'aws-amplify-react-native'
+import { AmplifyTheme, Localei18n } from './components'
+import awsconfig from '../aws-exports'
+
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true
+  }
+})
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password'
+    }
+  ]
+}
+
+const App = () => {
+  return (
+    <>
+      <Localei18n />
+      <StatusBar barStyle="dark-content" />
+      <Authenticator usernameAttributes="email" signUpConfig={signUpConfig} theme={AmplifyTheme} />
+    </>
+  )
+}
+
+export default App
