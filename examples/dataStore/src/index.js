8d73d1a4751fd14debab41496de5a7d468f80ae0
raoffonom <raoffonom@icloud.com>
2020-04-02 17:19:04 +0300

    UI Kit

--- examples/dataStore/src/index.js
+++ examples/dataStore/src/index.js
@@ -5,14 +5,25 @@
  * @format
  * @flow
  */
-
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome5'
+import React, { useState } from 'react'
+import { Switch, View } from 'react-native'
+import { ThemeProvider, DarkTheme, LightTheme } from 'react-native-unicorn-uikit'
+import UIKit from './UIKit'
 
 const App = () => {
+  const [value, setValue] = useState(!false)
+  const dev = true
+  const theme = value ? DarkTheme : LightTheme
   return (
     <>
-      <Icon name="comments" size={30} color="#900" />
+      <ThemeProvider theme={theme}>
+        {dev && (
+          <View style={{ backgroundColor: value ? '#1D1E1F' : '#fff', height: 90 }}>
+            <Switch onValueChange={setValue} value={value} style={{ alignSelf: 'center', marginTop: 50 }} />
+          </View>
+        )}
+        <UIKit />
+      </ThemeProvider>
     </>
   )
 }
